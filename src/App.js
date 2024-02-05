a0c096c123b391e4a5f25fe7ceb49cc2341acddb
Tigabu <tigabu.m@productera.io>
2024-02-05 15:18:58 +0300

    feat: starting code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <div className="headerLeft">
+          <div className="logo">Your Logo</div>
+          <div className="headerText">
+            <ul className="listItem">
+              <li className="list">Learn</li>
+              <li className="list">Teach</li>
+              <li className="list">Projects</li>
+              <li className="list">Stats</li>
+              <li className="list">Help Us</li>
+              <li className="list">Incubator</li>
+              <li className="list">About</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="buttons">
+          <button className="button create">Create</button>
+          <button className="button">Sign In</button>
+        </div>
       </header>
+      <div className="body">
+        <section className="topSection">
+          <div className="backgroundImage"></div>
+
+          <div className="text">
+            <h1 className="headerTopText">Help students become superheroes!</h1>
+            <p>
+              Join our highly sipportive Professional Learning <br /> Program
+              for middle and high school educators.
+            </p>
+            <button className="button">Click me</button>
+          </div>
+        </section>
+        <section className="middleSection">
+          <div className="imageWithText">
+            <img src="image1.jpg" alt="Image 1" />
+            <div className="imageText">Text for Image 1</div>
+          </div>
+          {/* Repeat the above structure for the other three images */}
+        </section>
+        <section className="bottomSection">
+          <div className="bottomText">Text for the bottom section</div>
+          <button className="button">Button</button>
+        </section>
+      </div>
+      <footer className="footer">Your Footer Content</footer>
     </div>
   );
-}
+};
 
 export default App;
