7500e738454949a20fe5332f333adbd086ea9764
Tigabu <tigabu.m@productera.io>
2024-02-15 18:20:45 +0300

    feat: image added(#02)

---
+++ src/images.js
@@ -0,0 +1,28 @@
+const images = [
+  {
+    id: 1,
+    src: "https://media.istockphoto.com/id/1344229806/photo/joyful-black-girl-studying-online-using-laptop-typing-on-keyboard.jpg?s=2048x2048&w=is&k=20&c=NN0UHzNxUtXZ_fMSO0Kz03he7BVoIetCi2RKzJB1aIE=",
+    title: "kidalonecode",
+    description: "kids alone can learn to code",
+  },
+  {
+    id: 2,
+    src: "https://media.istockphoto.com/id/1210945977/photo/male-high-school-pupil-building-robot-car-in-science-lesson.jpg?s=612x612&w=0&k=20&c=aR0qkyuJ6BZ66eY4bnZQzOaufDOPyU_04NckAuCDhb4=",
+    title: "kidsclassroomcode",
+    description: "kids can code together with friends in room",
+  },
+  {
+    id: 3,
+    src: "https://media.istockphoto.com/id/1144294233/photo/programmer-boy-coding-on-his-computer.jpg?s=612x612&w=0&k=20&c=NtI_nuVBUrVOzyQjEKJGk8UBTWCMTjBKzuewtF3OCGs=",
+    title: "kidcodealonehome",
+    description: "kids can learn coding alone in home",
+  },
+  {
+    id: 4,
+    src: "https://media.istockphoto.com/id/1047617072/photo/teacher-helping-female-pupil-line-of-high-school-students-working-at-screens-in-computer-class.jpg?s=612x612&w=0&k=20&c=oSXnkM8gqpNkML1qf4agChAziD9zropV9TZ_mhhJWgM=",
+    title: "kidscodewithinstructor",
+    description: "kids can learn coding with instructor",
+  },
+];
+
+export default images;
